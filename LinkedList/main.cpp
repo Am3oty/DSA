9f5546500024feee132b4d2ea7008ca9179d2d6e
Ismail Magdy <esmailmagdy573@gmail.com>
2025-05-05 12:19:57 +0300

    IsSorted

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -16,7 +16,8 @@ class SingleLinkedList {
     private: Node * Head;
     Node * Last;
     int Size = 0;
-    public: SingleLinkedList(int y) {
+    public:
+        SingleLinkedList(int y) {
             Head = new Node(y);
             Last = Head;
             Size++;
@@ -24,148 +25,169 @@ class SingleLinkedList {
         ~SingleLinkedList() {
             delete Head;
         }
-    void Append(int x) {
-        Last -> Next = new Node(x);
-        Last = Last -> Next;
-        Size++;
-    }
-    void Display() {
-        Node * Current = Head;
-        while (Current != nullptr) {
-            cout << Current -> data << endl;
-            Current = Current -> Next;
-        }
-    }
-    void Insert(int data, int position) {
-        if (position < 1) {
-            cout << "Invalid position!" << endl;
-            return;
-        }
 
-        Node * newNode = new Node(data);
-
-        if (position == 1) {
-            newNode -> Next = Head;
-            Head = newNode;
-            if (Size == 0) {
-                Last = Head;
-            }
+        void Append(int x) {
+            Last -> Next = new Node(x);
+            Last = Last -> Next;
             Size++;
-            return;
-        } else if (Head == NULL) {
-            Head = newNode;
-            Last = newNode;
-            Size++;
-        } else {
-            newNode -> Next = Head;
-            Head = newNode;
-            Size++;
-            return;
         }
 
-        Node * Current = Head;
-        for (int i = 1; i < position - 1 && Current != nullptr; ++i) {
-            Current = Current -> Next;
-        }
 
-        if (Current == nullptr) {
-            cout << "Position out of bounds!" << endl;
-            delete newNode;
-            return;
-        }
-
-        newNode -> Next = Current -> Next;
-        Current -> Next = newNode;
-        Size++;
-
-        if (newNode -> Next == nullptr) {
-            Last = newNode;
-        }
-    }
-    int LinearSearch(int ValueToSearch) {
-        Node * Current = Head;
-        for (int i = 1; i < Size + 1; i++) {
-            if (Current -> data == ValueToSearch)
-                return i;
-            else
-                Current = Current -> Next;
-
-        }
-
-        return -1;
-    }
-    void Sort() {
-        bool swapped;
-        Node * current;
-        Node * lastSorted = nullptr;
-
-        do {
-            swapped = false;
-            current = Head;
-            while (current -> Next != lastSorted) {
-
-                if (current -> data > current -> Next -> data) {
-                    // Swap the data
-                    int temp = current -> data;
-                    current -> data = current -> Next -> data;
-                    current -> Next -> data = temp;
-                    swapped = true;
-                }
-                current = current -> Next;
-
-            }
-
-            lastSorted = current;
-        } while (swapped);
-
-    }
-    void Delete(int position) {
-
-        if (position < 1 || position > Size) {
-            cout << "Invalid Position\n";
-            return;
-        }
-
-        if (position == 1 && Size == 1) {
-            Head -> data = NULL;
-            Size--;
-            return;
-
-        } else if (position == 1) {
-            Node * temp = Head;
-            Head = Head -> Next;
-            temp -> Next = nullptr; // Important to prevent recursive deletion
-            delete temp;
-            Size--;
-            if (Size == 0) Last = nullptr;
-            return;
-        } else if (position == Size) { //Delete Last Node
-
+        void Display() {
             Node * Current = Head;
-            while (Current -> Next -> Next != nullptr) {
+            while (Current != nullptr) {
+                cout << Current -> data << endl;
                 Current = Current -> Next;
             }
+        }
 
-            Last = Current;
-            delete Last -> Next;
-            Last -> Next = nullptr;
-            Size--;
-            return;
-        } else {
+
+        void Insert(int data, int position) {
+            if (position < 1) {
+                cout << "Invalid position!" << endl;
+                return;
+            }
+
+            Node * newNode = new Node(data);
+
+            if (position == 1) {
+                newNode -> Next = Head;
+                Head = newNode;
+                if (Size == 0) {
+                    Last = Head;
+                }
+                Size++;
+                return;
+            } else if (Head == NULL) {
+                Head = newNode;
+                Last = newNode;
+                Size++;
+            }
 
             Node * Current = Head;
             for (int i = 1; i < position - 1 && Current != nullptr; ++i) {
                 Current = Current -> Next;
             }
 
-            Node * Temp = Current -> Next -> Next;
-            Current -> Next -> Next = nullptr;
-            delete Current -> Next;
-            Current -> Next = Temp;
-            Size--;
+            if (Current == nullptr) {
+                cout << "Position out of bounds!" << endl;
+                delete newNode;
+                return;
+            }
+
+            newNode -> Next = Current -> Next;
+            Current -> Next = newNode;
+            Size++;
+
+            if (newNode -> Next == nullptr) {
+                Last = newNode;
+            }
+        }
+
+
+        int LinearSearch(int ValueToSearch) {
+            Node * Current = Head;
+            for (int i = 1; i < Size + 1; i++) {
+                if (Current -> data == ValueToSearch)
+                    return i;
+                else
+                    Current = Current -> Next;
+
+            }
+
+            return -1;
+        }
+
+
+        void Sort() {
+            bool swapped;
+            Node * current;
+            Node * lastSorted = nullptr;
+
+            do {
+                swapped = false;
+                current = Head;
+                while (current -> Next != lastSorted) {
+
+                    if (current -> data > current -> Next -> data) {
+                        // Swap the data
+                        int temp = current -> data;
+                        current -> data = current -> Next -> data;
+                        current -> Next -> data = temp;
+                        swapped = true;
+                    }
+                    current = current -> Next;
+
+                }
+
+                lastSorted = current;
+            } while (swapped);
 
         }
-    }
+
+        void Delete(int position) {
+
+            if (position < 1 || position > Size) {
+                cout << "Invalid Position\n";
+                return;
+            }
+
+            if (position == 1 && Size == 1) {
+                Head -> data = NULL;
+                Size--;
+                return;
+
+            } else if (position == 1) {
+                Node * temp = Head;
+                Head = Head -> Next;
+                temp -> Next = nullptr; // Important to prevent recursive deletion
+                delete temp;
+                Size--;
+                if (Size == 0) Last = nullptr;
+                return;
+            } else if (position == Size) { //Delete Last Node
+
+                Node * Current = Head;
+                while (Current -> Next -> Next != nullptr) {
+                    Current = Current -> Next;
+                }
+
+                Last = Current;
+                delete Last -> Next;
+                Last -> Next = nullptr;
+                Size--;
+                return;
+            } else {
+
+                Node * Current = Head;
+                for (int i = 1; i < position - 1 && Current != nullptr; ++i) {
+                    Current = Current -> Next;
+                }
+
+                Node * Temp = Current -> Next -> Next;
+                Current -> Next -> Next = nullptr;
+                delete Current -> Next;
+                Current -> Next = Temp;
+                Size--;
+
+            }
+        }
+
+        bool IsSorted()
+        {
+            Node *Current = Head;
+
+            while(Current->Next!=nullptr)
+            {
+                if(Current->data > Current->Next->data)
+                    return false;
+                else
+                {
+                    Current=Current->Next;
+                }
+            }
+            return true;
+        }
 };
 
 int main() {
@@ -179,10 +201,19 @@ int main() {
     cout << "\nInserting 29 at position 2...\n";
     mylist.Insert(29, 2);
     mylist.Display();
-
+    cout << "\Checking if the list is sorted...\n";
+    if(mylist.IsSorted())
+        cout<<"List is Sorted\n";
+    else
+        cout<<"List is not sorted\n";
     cout << "\Sorting the list...\n";
     mylist.Sort();
     mylist.Display();
+        cout << "\Checking if the list is sorted...\n";
+    if(mylist.IsSorted())
+        cout<<"List is Sorted\n";
+    else
+        cout<<"List is not sorted\n";
     cout << "\Searching the list...\n";
     int position = mylist.LinearSearch(29);
     if (position > 0)
